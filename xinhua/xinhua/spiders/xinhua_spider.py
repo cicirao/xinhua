a86e603c53ddd9c3faa911b55e477d609e8b8834
Xi Rao <cicirao.hust@gmail.com>
2014-11-10 15:47:52 -0600

    write code to go through multi-pagesweb site

--- xinhua/xinhua/spiders/xinhua_spider.py
+++ xinhua/xinhua/spiders/xinhua_spider.py
@@ -13,7 +13,7 @@ class XinhuaSpider(scrapy.Spider):
   name = "xinhua"
   download_delay = 1 
   allowed_domains = ["info.search.news.cn"]
-  start_urls = ["http://info.search.news.cn/result.jspa?pno=23&rp=40&t1=0&btn=%CB%D1+%CB%F7&t=1&n1=%D7%D4%C8%BB%D6%AE%D3%D1&np=1&ct=%D7%D4%C8%BB%D6%AE%D3%D1&ss=2"]
+  start_urls = ["http://info.search.news.cn/result.jspa?pno=3&rp=40&t1=0&btn=%CB%D1+%CB%F7&t=1&n1=%D7%D4%C8%BB%D6%AE%D3%D1&np=1&ct=%D7%D4%C8%BB%D6%AE%D3%D1&ss=2"]
   # def start_requests(self):
   #   for i in xrange(31, 41):
   #     yield self.make_requests_from_url("http://info.search.news.cn/result.jspa?pno=%d&rp=40&t1=0&btn=%%CB%%D1+%%CB%%F7&t=1&n1=%%D7%%D4%%C8%%BB%%D6%%AE%%D3%%D1&np=1&ct=%%25%%3F%%25%%3F%%25%%3F%%25%%3F%%25%%3F%%25%%3F%%25%%3F%%25%%3F&ss=2" % i)
@@ -32,33 +32,7 @@ class XinhuaSpider(scrapy.Spider):
       item['published_at'] = published_at
 
       try: # filter available websites
-        d = urllib.urlopen(link)
-        if d.getcode() == 200: # check if valid
-          data = d.read()  
-          r = requests.get(link)
-          if r.encoding.lower() != 'utf-8' and r.encoding.lower() != 'gbk': # convert charset
-            data = data.decode('gb2312').encoding('utf-8')
-          elif r.encoding.lower() == 'gbk':
-            data = data.decode('gbk').encoding('utf-8')
-
-          hxs = Selector(text=data)
-          
-          if hxs.xpath('//p').extract(): # get p node text
-            content = ''.join(hxs.xpath('//p//text()').extract()).strip()
-
-          elif link.startswith('http://www.p5w.net/'): # special website with no p tag
-            content = ''.join(hxs.xpath('//div[@class="new_content_p BSHARE_POP"]//text()').extract()).strip()
-          elif link.startswith('http://hy.stock.cnfol.com/') or link.startswith('http://news.cnfol.com/'):
-            content = ''.join(hxs.xpath('//div[@id="__content"]//text()').extract()).strip()
-          elif link.startswith('http://mzl.wenming.cn/'):
-            content = ''.join(hxs.xpath('//div[@class="Custom_UnionStyle"]//text()').extract()).strip()  
-
-          else: # just in case
-            content = ["no p tag"]
-          
-          item['content'] = content # pass value to the instance
-        else:
-          item['content'] = "can not load page"
+        self.parse_detail(link)
 
       except Exception,e:
         item['content'] = "This webpage is not available"
@@ -71,6 +45,65 @@ class XinhuaSpider(scrapy.Spider):
     print url
     yield Request(url, callback=self.parse)
 
+  # parse each url
+  def parse_detail(self, link):
+    d = urllib.urlopen(link)
+    if d.getcode() == 200: # check if valid
+      data = d.read()  
+      r = requests.get(link)
+      if r.encoding.lower() != 'utf-8': # convert charset
+        data = data.decode('gbk').encoding('utf-8')
+
+      hxs = Selector(text=data) # get page data
+
+      if hxs.xpath('//p').extract(): # get p node text
+        content = ''.join(hxs.xpath('//p//text()').extract()).strip()
+
+      elif link.startswith('http://www.p5w.net/'): # special website with no p tag
+        content = ''.join(hxs.xpath('//div[@class="new_content_p BSHARE_POP"]//text()').extract()).strip()
+      elif link.startswith('http://hy.stock.cnfol.com/') or link.startswith('http://news.cnfol.com/'):
+        content = ''.join(hxs.xpath('//div[@id="__content"]//text()').extract()).strip()
+      elif link.startswith('http://mzl.wenming.cn/'):
+        content = ''.join(hxs.xpath('//div[@class="Custom_UnionStyle"]//text()').extract()).strip()  
+
+      else: # just in case
+        content = ["no p tag"]
+      
+      item['content'] = content # pass value to the instance
+
+      if hxs.xpath('//div[@id="div_currpage"]').extract(): # check if multiple pages
+        for page in hxs.xpath('//div[@id="div_currpage"]/a/@href').extract()[:-1]: # get all links except the last one
+          # get content
+          print page
+          p = urllib.urlopen(page)
+          data_p = p.read()
+          r_p = requests.get(page)
+          if r_p.encoding.lower() != 'utf-8': # convert charset
+            data_p = data_p.decode('gbk').encoding('utf-8')
+
+          hxs_p = Selector(text=data_p) # get page data
+
+          if hxs_p.xpath('//p').extract(): # get p node text
+            content = ''.join(hxs_p.xpath('//p//text()').extract()).strip()
+
+          elif page.startswith('http://www.p5w.net/'): # special website with no p tag
+            content = ''.join(hxs_p.xpath('//div[@class="new_content_p BSHARE_POP"]//text()').extract()).strip()
+          elif page.startswith('http://hy.stock.cnfol.com/') or page.startswith('http://news.cnfol.com/'):
+            content = ''.join(hxs_p.xpath('//div[@id="__content"]//text()').extract()).strip()
+          elif page.startswith('http://mzl.wenming.cn/'):
+            content = ''.join(hxs_p.xpath('//div[@class="Custom_UnionStyle"]//text()').extract()).strip()  
+
+          else: # just in case
+            content = ["no p tag"]
+
+          item['content'].append(content)     
+
+    else:
+      item['content'] = "can not load page"
+
+    yield item['content']
+
+# clean up all tags
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
